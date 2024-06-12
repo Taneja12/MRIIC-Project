deb0615019bb136f9fd44644f29e7a99be6a4706
Deepanshu Taneja <deepanshutaneja762@gmail.com>
2024-06-12 12:19:11 +0530

    .

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const express = require('express')
+var jwt = require('jsonwebtoken')
+var cookieParser = require('cookie-parser')
 const app = express()
 var bodyParser = require('body-parser')
 const bcrypt = require('bcrypt');
@@ -8,10 +10,11 @@ const { MongoClient } = require('mongodb');
 
 // Connection URL
 const url = process.env.MONGO_URL
-const client = new MongoClient(url);
+const client = new MongoClient(url)
+const dbName = 'mriirs';
+let db;
 
 // Database Name
-const dbName = 'mriirs';
 const multer = require('multer')
 app.use('/uploads', express.static('uploads'))
 
@@ -38,7 +41,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
-  res.render('home')
+  res.render('home', {
+    isAuthenticated: false
+  })
 })
 
 app.get('/admin', async function (req, res) {
@@ -93,45 +98,90 @@ app.post('/', upload.single('uploaded_file'), function (req, res) {
   res.redirect('/')
 })
 
-app.post('/signup', function (req, res) {
+app.post('/signup', async function (req, res) {
   let a = req.body['user_email']
   let b = req.body['user_pwd1']
   let c = req.body['user_pwd2']
-  if (b === c) {
-    console.log(a, b)
-    bcrypt.hash(b, saltRounds, function (err, hash) {
-      let z = hash
-      console.log(z)
-      async function store_user() {
-        // Use connect method to connect to the server
-        try {
-          await client.connect();
-          console.log('Connected successfully to server');
-          const db = client.db(dbName);
-          const collection = db.collection('users');
-          // the following code examples can be pasted here...
-          let result = await collection.insertOne({ email: a, password: z })
-          return 'done.';
-        } catch (e) {
-          console.error(e);
-        }
-      }
 
-      store_user()
-        .then(console.log)
-        .catch(console.error)
-        .finally(() => client.close());
-      res.redirect('/')
-      // Store hash in your password DB.
-    });
+  if (b != c)
+    return res.send("Passwords dont match!")
+
+  console.log("fun start")
+  // Use connect method to connect to the server
+  try {
+    const collection = db.collection('users');
+    const user = await collection.findOne({ email: a })
+    console.log(user)
+
+    if (user) {
+      return res.send('User already')
+    }
+
+    const hashedPassword = await bcrypt.hash(b, 10)
+    await collection.insertOne({ email: a, password: hashedPassword })
+    return res.redirect('/')
+    // the following code examples can be pasted here...
+  } catch (e) {
+    console.error(e);
+    return res.send("There was an error!")
   }
-  else {
-    res.send('passwords do not match')
-  }
+
 })
 
 app.get('/signup', function (req, res) {
   res.sendFile(__dirname + '/views/signup.html')
 })
 
-app.listen(3000)
+app.get('/login', function (req, res) {
+  res.sendFile(__dirname + '/views/login.html')
+})
+
+app.post('/login', async function (req, res) {
+  let a = req.body['user_email']
+  let b = req.body['user_pwd']
+
+  try {
+    const collection = db.collection('users');
+    const user = await collection.findOne({ email: a })
+
+    console.log(user.password, b)
+
+    const isPasswordValid = await bcrypt.compare(b, user.password)
+    if (!isPasswordValid)
+      return res.send("Invalid password")
+
+    const token = jwt.sign({ email: a }, 'shhhhh');
+
+    res.cookie("auth_token", token)
+    res.render('home', {
+      isAuthenticated: true
+    })
+  }catch(er){
+    console.log(er)
+    return res.send("Error")
+  }
+
+
+  
+})
+
+app.get('/logout', (req, res) => {
+  res.clearCookie('auth_token')
+  res.redirect('/login')
+})
+
+app.use(cookieParser())
+
+app.get('/', function (req, res) {
+  // Cookies that have not been signed
+  console.log('Cookies: ', req.cookies)
+
+  // Cookies that have been signed
+  console.log('Signed Cookies: ', req.signedCookies)
+})
+
+app.listen(3000, async () => {
+  await client.connect().then(() => {
+    db = client.db(dbName);
+  })
+})
